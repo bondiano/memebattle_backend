0c3b200a37693aed44489405e7ca004a74175883
bondiano <batbondik0@gmail.com>
2017-11-25 23:31:22 +0700

    typo

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,9 @@
 const auth = require('./auth');
-const service = require('./service');
 const game = require('./game');
 
 module.exports = (app, db) => {
 
     auth(app, db);
-    service(app, db);
     game(app, db);
 
     app.get('/', (req, res) => {
