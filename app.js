f793ddf057264f729e56cbdbeb102e39e6920968
Artem Zverev <themezv@gmail.com>
2018-02-15 21:03:49 +0700

    add mongo, move redis to bootstrap

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-const app = require('./bootstrap');
+require('dotenv').config({path: './config/.env'});
+const {HTTP_PORT: port} = process.env;
 
-app.server.listen(app.port, () => {
-  console.log(`App listening on port ${app.port}!`);
+const cors = require('cors');
+const pgdb = require('./db/pg-db');
+const bodyParser = require('body-parser');
+
+
+
+/* Middleware init section */
+const app = require('express')();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server, {path: '/socket.io', transports: ['websockets', 'polling'], secure: true});
+
+/* Middleware use section */
+app.use(cors());
+app.use(bodyParser.json());
+
+/* Set up app routes */
+require('./routes')(app, pgdb);
+require('./ws')(io);
+
+app.listen(port, () => {
+  console.log(`App listening on port ${port}!`);
 });
