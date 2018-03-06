a82255c6acc946fabf4fabfd636cd1d5b9c74f58
thekudryash <kudryash1997@mail.ru>
2018-03-07 01:34:35 +0700

    Add postgresql configs

---
+++ config/configs.js
@@ -0,0 +1,12 @@
+// PostgreSQL configs
+const PG_HOST = process.env.PG_HOST;
+const PG_DB = process.env.PG_DB;
+const PG_USER = process.env.PG_USER;
+const PG_PASSWORD = process.env.PG_PASSWORD;
+
+module.exports = {
+    PG_HOST,
+    PG_DB,
+    PG_USER,
+    PG_PASSWORD,
+};
