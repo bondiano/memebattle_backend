9fa9053a3079c80f39d725d139951c3e19dcd8da
Vassiliy Kuzenkov <vassiliy.kuzenkov@noveogroup.com>
2018-05-14 16:42:55 +0700

    Little cleanup to one code style

--- app/controllers/auth.controller.js
+++ app/controllers/auth.controller.js
@@ -1,17 +1,21 @@
 const {CAS: {CAS_URL, PARTNER_ID}} = require('../config/config');
 
+const socialNetwork = (req, res) => {
+    const {provider} = res.params;
+
+    return res.redirect(`${CAS_URL}/auth/${provider}?partner_id=${PARTNER_ID}`);
+};
+
+const registration = (req, res) => {
+    return res.success();
+};
+
+const login = (req, res) => {
+    return res.success();
+};
+
 module.exports = {
-    socialNetwork(req, res) {
-        const {provider} = res.params;
-
-        return res.redirect(`${CAS_URL}/auth/${provider}?partner_id=${PARTNER_ID}`);
-    },
-
-    registration(req, res) {
-        return res.success();
-    },
-
-    login(req, res) {
-        return res.success();
-    }
+    socialNetwork,
+    registration,
+    login
 };
